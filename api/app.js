b4b295748896d7a21551d1c0e5628f773651e0cc
sujanchaudhary415 <sujanchaudhary415@gmail.com>
2024-10-03 13:42:25 +0545

    worked on user register and added cors

--- api/app.js
+++ api/app.js
@@ -1,8 +1,16 @@
 import express from "express";
+import cors from "cors"; // Importing the cors module
 import cookieParser from "cookie-parser"
 import authRoute from "./routes/auth.route.js"
 
 const app=express();
+
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from the frontend running on this origin
+    credentials: true, // Allow credentials (cookies, etc.)
+}));
+
 
 app.use(express.json())
 app.use(cookieParser())
--- client/src/routes/Register/Register.jsx
+++ client/src/routes/Register/Register.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.scss";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 const Register = () => {
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+      //setError()
+    }
+  };
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />

abe1c460275419b4a6a1dc019079608786418488
sujanchaudhary415 <sujanchaudhary415@gmail.com>
2024-10-03 19:26:45 +0545

    user Register debugged

--- api/app.js
+++ api/app.js
@@ -1,24 +1,22 @@
 import express from "express";
-import cors from "cors"; // Importing the cors module
-import cookieParser from "cookie-parser"
-import authRoute from "./routes/auth.route.js"
+import cors from "cors"; 
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
 
-const app=express();
 
+
+const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Allow requests from the frontend running on this origin
-    credentials: true, // Allow credentials (cookies, etc.)
+    origin: process.env.CLIENT_URL, // Make sure this is set correctly
+    credentials: true, // Allow cookies to be sent
 }));
 
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(cookieParser())
+app.use("/api/auth", authRoute);
 
-app.use("/api/auth",authRoute)
-
-
-
-app.listen(8800,()=>{
-    console.log("Server is running ")
-})
+app.listen(8800, () => {
+    console.log("Server is running");
+});

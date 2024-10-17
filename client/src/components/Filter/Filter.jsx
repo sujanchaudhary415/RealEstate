bc9c043ccbeed0a6816baa675fb0bbc57d6d84ef
sujanchaudhary415 <sujanchaudhary415@gmail.com>
2024-10-17 08:27:51 +0545

    added search functionality and improved filter

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import { useState } from "react";
 import "./Filter.scss";
-const Filter = () => {
+import { useSearchParams } from "react-router-dom";
+
+function Filter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    property: searchParams.get("property") || "",
+    minPrice: searchParams.get("minPrice") || "",
+    maxPrice: searchParams.get("maxPrice") || "",
+    bedroom: searchParams.get("bedroom") || "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setQuery({
+      ...query,
+      [name]: value,
+    });
+  };
+
+  const handleFilter = () => {
+    const updatedParams = { ...query };
+    
+    // Remove any empty fields from search params
+    Object.keys(updatedParams).forEach(key => {
+      if (!updatedParams[key]) {
+        delete updatedParams[key];
+      }
+    });
+
+    setSearchParams(updatedParams);
+  };
+
   return (
     <div className="filter">
       <h1>
-        Search results for <b>London</b>
+        Search results for <b>{searchParams.get("city")}</b>
       </h1>
       <div className="top">
         <div className="item">
@@ -14,22 +47,34 @@ const Filter = () => {
             id="city"
             name="city"
             placeholder="City Location"
+            onChange={handleChange}
+            value={query.city} 
           />
         </div>
       </div>
       <div className="bottom">
         <div className="item">
           <label htmlFor="type">Type</label>
-          <select name="type" id="type">
-          <option value="any">any</option>
+          <select
+            name="type"
+            id="type"
+            onChange={handleChange}
+            value={query.type} 
+          >
+            <option value="">any</option>
             <option value="buy">Buy</option>
             <option value="rent">Rent</option>
           </select>
         </div>
         <div className="item">
-          <label htmlFor="Property">Property</label>
-          <select name="property" id="property">
-          <option value="">any</option>
+          <label htmlFor="property">Property</label>
+          <select
+            name="property"
+            id="property"
+            onChange={handleChange}
+            value={query.property}  
+          >
+            <option value="">any</option>
             <option value="apartment">Apartment</option>
             <option value="house">House</option>
             <option value="condo">Condo</option>
@@ -43,32 +88,38 @@ const Filter = () => {
             id="minPrice"
             name="minPrice"
             placeholder="any"
+            onChange={handleChange}
+            value={query.minPrice}  
           />
         </div>
         <div className="item">
           <label htmlFor="maxPrice">Max Price</label>
           <input
-            type="text"
+            type="number"
             id="maxPrice"
             name="maxPrice"
             placeholder="any"
+            onChange={handleChange}
+            value={query.maxPrice}  
           />
         </div>
         <div className="item">
           <label htmlFor="bedroom">Bedroom</label>
           <input
-            type="text"
+            type="number"
             id="bedroom"
             name="bedroom"
             placeholder="any"
+            onChange={handleChange}
+            value={query.bedroom} 
           />
         </div>
-        <button>
+        <button onClick={handleFilter}>
           <img src="/search.png" alt="" />
         </button>
       </div>
     </div>
   );
-};
+}
 
 export default Filter;
--- client/src/routes/ListPage/ListPage.jsx
+++ client/src/routes/ListPage/ListPage.jsx
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./ListPage.scss";
 import Filter from "../../components/Filter/Filter";
 import Card from "../../components/Card/Card";
 import Map from "../../components/Map/Map";
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
 const ListPage = () => {
-const posts =useLoaderData()
+const data =useLoaderData()
   return (
     <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
-          {posts.map(item=>(
-            <Card key={item.id}  item={item} />
+         <Suspense fallback={<p>Loading...</p>}>
+         <Await
+          resolve={data.postResponse}
+          errorElement={
+            <p>Error loading posts!</p>
+          }
+        >
+          {(postResponse) => postResponse.data.map(post=>(
+            <Card key={post.id} item={post} />
           ))}
+        </Await>
+           
+         </Suspense>
         </div>
       </div>
       <div className="mapContainer">
-        <Map items={posts}/>
+        {
+           <Suspense fallback={<p>Loading...</p>}>
+           <Await
+            resolve={data.postResponse}
+            errorElement={
+              <p>Error loading posts!</p>
+            }
+          >
+            {(postResponse) => 
+             
+              <Map items={postResponse.data}/>
+            }
+          </Await>
+        </Suspense>
+        }
       </div>
     </div>
   );

cf5fede0735bd01a5d63bd24e9c47e91e1c4ddf0
sujanchaudhary415 <sujanchaudhary415@gmail.com>
2024-10-20 16:15:08 +0545

    added chat functionality

--- api/app.js
+++ api/app.js
@@ -4,7 +4,9 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
-import postRoute from "./routes/post.route.js"
+import postRoute from "./routes/post.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
@@ -21,7 +23,9 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/test", testRoute);
-app.use("/api/posts",postRoute)
+app.use("/api/posts",postRoute);
+app.use("/api/chats",chatRoute);
+app.use("/api/messages",messageRoute)
 
 app.listen(8800, () => {
   console.log("Server is running");

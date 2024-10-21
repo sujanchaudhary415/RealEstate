7b80d11883967359c5f3ca55b5bf680226dafb02
sujanchaudhary415 <sujanchaudhary415@gmail.com>
2024-10-21 10:50:19 +0545

    zustand added

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,16 +1,25 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
-const Navbar = () => {
+import { useNotificationStore } from "../../lib/notificationStore";
+
+function Navbar() {
   const [open, setOpen] = useState(false);
-  const {currentUser}=useContext(AuthContext)
+
+  const { currentUser } = useContext(AuthContext);
+
+  const fetch = useNotificationStore((state) => state.fetch);
+  const number = useNotificationStore((state) => state.number);
+
+  if(currentUser) fetch();
+
   return (
     <nav>
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>ForeverEstate</span>
+          <span>LamaEstate</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
@@ -20,14 +29,11 @@ const Navbar = () => {
       <div className="right">
         {currentUser ? (
           <div className="user">
-            <img
-              src={currentUser.avatar || "/noavatar.jpg"}
-              alt=""
-            />
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
             <span>{currentUser.username}</span>
             <Link to="/profile" className="profile">
-            <div className="notification">3</div>
-            <span>Profile</span>
+              {number > 0 && <div className="notification">{number}</div>}
+              <span>Profile</span>
             </Link>
           </div>
         ) : (
@@ -39,7 +45,11 @@ const Navbar = () => {
           </>
         )}
         <div className="menuIcon">
-          <img src="/menu.png" alt="" onClick={() => setOpen(!open)} />
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
         </div>
         <div className={open ? "menu active" : "menu"}>
           <a href="/">Home</a>
@@ -52,6 +62,6 @@ const Navbar = () => {
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
